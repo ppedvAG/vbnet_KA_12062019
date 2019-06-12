039b2997248b3c5594951ad0889dee238cf01cfc
André <andrerrr@outlook.de>
2019-06-12 16:02:51 +0200

    DVD sind fast neu

---
+++ DVDScreensaver/DVDScreensaver/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Aqua
+        Me.Button1.Font = New System.Drawing.Font("Showcard Gothic", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(284, 186)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(179, 93)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "DVD"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TrackBar1.Location = New System.Drawing.Point(208, 382)
+        Me.TrackBar1.Minimum = 1
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(580, 56)
+        Me.TrackBar1.TabIndex = 1
+        Me.TrackBar1.Value = 5
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(366, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Button2 As Button
+End Class
---
+++ DVDScreensaver/DVDScreensaver/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+
+    Dim toLeft As Integer = 5
+    Dim toDown As Integer = 5
+    Dim ran As Random = New Random()
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Button1.Left = Button1.Left + toLeft
+
+        If Button1.Left + Button1.Width >= ClientSize.Width Or Button1.Left <= 0 Then
+            toLeft = toLeft * -1
+            MakeFun()
+        End If
+
+        Button1.Top = Button1.Top + toDown
+
+        If Button1.Top + Button1.Height >= ClientSize.Height Or Button1.Top <= 0 Then
+            toDown = toDown * -1
+            MakeFun()
+        End If
+    End Sub
+
+    Sub MakeFun()
+        Task.Run(Sub() Console.Beep(360, 80))
+
+
+        Dim newColor = Color.FromArgb(ran.Next(256), ran.Next(256), ran.Next(256))
+        Button1.BackColor = newColor
+
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        toLeft = TrackBar1.Value
+        toDown = TrackBar1.Value
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MakeFun()
+    End Sub
+
+
+    Private Sub Button2_MouseMove(sender As Object, e As MouseEventArgs) Handles Button2.MouseMove
+        'Button2.Left = Button2.Left + 5
+        Button2.Left += ran.Next(-50, 50)
+        Button2.Top += ran.Next(-20, 20)
+    End Sub
+End Class
