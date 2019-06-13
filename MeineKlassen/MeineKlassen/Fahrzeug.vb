83c6a050ef7223fe0e49f010d4f0cbfc58ea272c
André <andrerrr@outlook.de>
2019-06-13 14:25:20 +0200

    Meine Klassen

---
+++ MeineKlassen/MeineKlassen/Fahrzeug.vb
@@ -0,0 +1,13 @@
+﻿Public Class Fahrzeug
+
+    Public Property Farbe As String
+    Public Property Hersteller As String
+    Public Property Modell As String
+    Public Property Türen As Integer
+    Public Property Leistung As Integer
+
+    Public Sub Hupen()
+        Console.Beep()
+    End Sub
+
+End Class
